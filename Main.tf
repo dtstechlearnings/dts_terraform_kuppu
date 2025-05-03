b3e6129daa35071e62d38c0fbcc4255111c997b5
kuppu <skrajmca.us@gmail.com>
2025-05-03 15:57:56 -0500

    Firtst commit for the git

---
+++ Main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  //instance_type = values(var.ec2_instance_type_map)[count.index]
+  count =1
+  instance_type = "t2.micro"
+  ami           = var.ec2_ami_id // Replace with a valid AMI ID
+  lifecycle {
+     prevent_destroy = true
+  }
+}
---
+++ Variables.tf
@@ -0,0 +1,4 @@
+variable "ec2_ami_id" {
+//efault = "ami-08b5b3a93ed654d19" 
+default  = "ami-04aa00acb1165b32a"
+}
